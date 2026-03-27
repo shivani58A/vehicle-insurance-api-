2428cb588d62a89dabdb1c2febab01e8611f343d
Shivani Devi <71819053+shivani58A@users.noreply.github.com>
2026-03-27 12:10:02 +0530

    Added Customer model with properties

---
+++ Models/Customer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string LicenseNumber { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        public virtual ICollection<Policy> Policies { get; set; }
+    }
+}
